fbb0eb5a6dcb54642de7b831c786a4d8653fdbd8
arx_247 <ar_nas@kkumail.com>
2021-07-30 00:26:14 +0700

    Add P4.py

--- P3.py
+++ P3.py
@@ -2,3 +2,19 @@
 """
 Lab 2 Variables and Collections: Problem 3
 """
+lit1 = [1, 2, 3, 4, 5]
+lit2 = lit1
+def add_number_to_list(numList):
+    add = input("Enter a number to add to a list: ")
+    list1.extend(["%s" % (add)])
+    
+
+
+
+if __name__ == '__main__':
+    lit1 = [1, 2, 3, 4, 5]
+    print(add_number_to_list(numList))
+
+ #relace_number_in_list()
+    #remove_number_in_list()
+
