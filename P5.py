ee96e9dcd6950bb24ff515d93b8139748c32968f
arx_247 <ar_nas@kkumail.com>
2021-07-30 01:25:18 +0700

    Add P5.py

--- P5.py
+++ P5.py
@@ -2,3 +2,10 @@
 """
 Lab 2 Variables and Collections: Problem 5
 """
+
+d = {'Gold':{'Thailand'},'Silver':{'Spain'},'Bronze':{'Israel'},'Bronze':{'Serbia'}}
+d['mynewkey'] = 'mynewvalue'
+print(d)
+
+
+
