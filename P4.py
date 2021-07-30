6025535840e92f1868ab358b92fc9438d4bdac37
arx_247 <ar_nas@kkumail.com>
2021-07-30 10:48:58 +0700

    Update P4.py

--- P4.py
+++ P4.py
@@ -5,6 +5,7 @@ Lab 2 Variables and Collections: Problem 4
 def Check_vowels(string, vowels):
     final = [each for each in string if each in vowels]
     print("Vowels in %s are %s"%(string,final))
+
 string = input("Enter string input: ")
 vowels = "AaEeIiOoUu"
 Check_vowels(string, vowels)
