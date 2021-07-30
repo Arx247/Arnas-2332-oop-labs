61f6ac9b55b18132b2e34f761715a329a5e0984a
arx_247 <ar_nas@kkumail.com>
2021-07-30 10:34:08 +0700

    Add P1.py

--- P6.py
+++ P6.py
@@ -1,4 +1,23 @@
-#Arnas Oonsadao, 633040233-2
+# Arnas Oonsadao, 633040233-2
 """
 Lab 2 Variables and Collections: Problem 6
 """
+\
+def split_string(string):
+    # Split the string based on space delimiter
+    list_string = string.split('')
+
+    return list_string
+
+
+def join_string(list_string):
+    # Join the string based on '-' delimiter
+    string = ' '.join(list_string)
+
+    return string
+olympics2020_str.split()
+if __name__ == '__main__':
+    m = olympics2020_str.split()
+    new_string = join_string(list_string)
+    print("For some Olympics 2020 games of Thai athletes: ")
+    print((new_string))
