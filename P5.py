748602f26e053acb9355fee9a0e6d730a5c94ca3
arx_247 <ar_nas@kkumail.com>
2021-07-30 11:37:54 +0700

    Update P5.py

--- P5.py
+++ P5.py
@@ -3,6 +3,7 @@
 Lab 2 Variables and Collections: Problem 5
 """
 taekwondo_olympics2020_w49k = {
+
     "Gold": {"Thailand"},
     "Silver": {"Spain"},
     "Bronze": {"Israel","Serbia"}
@@ -10,8 +11,8 @@ taekwondo_olympics2020_w49k = {
 print("The list of medals in Taekwondo Olympics 2020 women 49 kilograms:")
 for key, value in taekwondo_olympics2020_w49k.items():
     for items in value:
-        print("%s received %s medal"%(items,key))
-print("The dictionary of medals is",taekwondo_olympics2020_w49k)
+        print("%s received %s medal"%(items, key))
+print("The dictionary of medals is", taekwondo_olympics2020_w49k)
 
 
 
