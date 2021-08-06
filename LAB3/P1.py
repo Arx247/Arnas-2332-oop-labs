874ea87a8e3f5ae0b85e90ae3cf0e9c1f8c69c9c
arx_247 <ar_nas@kkumail.com>
2021-08-06 09:10:56 +0700

    Update

--- LAB3/P1.py
+++ LAB3/P1.py
@@ -4,22 +4,23 @@ Lab 3 Loops and Functional Programming Functions : Problem 1
 """
 
 import random
-n = random.randint(1, 10)
-guess = 5
+
+random_randint = random.randint(1, 10)
+MAX_NUM_GUESSES = 5
 def guess_remaining():
-    if guess > 1:
-        print("You have %s guess remaining"%(guess))
+    if MAX_NUM_GUESSES > 1:
+        print("You have %s guess remaining"%(MAX_NUM_GUESSES))
     else:
-        print("You have %s guess remaining"%(guess))
+        print("You have %s guess remaining"%(MAX_NUM_GUESSES))
 
 for i in range(5):
     integer = int(input("Enter a integer to guess:"))
-    if integer < n:
-        guess = guess - 1
+    if integer < random_randint:
+        MAX_NUM_GUESSES = MAX_NUM_GUESSES - 1
         print("Try a higher number.")
         guess_remaining()
-    elif integer > n:
-        guess = guess - 1
+    elif integer > random_randint:
+        MAX_NUM_GUESSES = MAX_NUM_GUESSES - 1
         print("Try a lower number.")
         guess_remaining()
     else:
